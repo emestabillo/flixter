9660d20fd10a0fe8c9cbd8d5ccd825761a957ed6
Emmilie Estabillo <emsestabillo@gmail.com>
2017-02-11 14:44:52 +0000

    Listing the courses

--- db/schema.rb
+++ db/schema.rb
@@ -25,14 +25,6 @@ ActiveRecord::Schema.define(version: 20170211043439) do
     t.index ["user_id"], name: "index_courses_on_user_id", using: :btree
   end
 
-  create_table "sections", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "course_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["course_id"], name: "index_sections_on_course_id", using: :btree
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
