040e27a735d820bef8fbbd564f1914260b9a72b4
Emmilie Estabillo <emsestabillo@gmail.com>
2017-03-04 16:05:06 +0000

    Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def show
   end
 
   private
 
+  def require_authorized_for_current_course
+    if ! current_user.enrolled_in?(current_course) ####mentor throwing an exception?
+      redirect_to course_path(current_course), alert: 'User not Enrolled'   
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  helper_method :current_course
+  def current_course
+    current_lesson.section.course
   end
 end
